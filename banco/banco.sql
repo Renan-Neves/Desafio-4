36faca2fd67c2581778e9292b88e2682faaa7277
Renan Neves <renannovo99@gmail.com>
2022-11-30 20:13:03 -0300

    socorro

--- banco/banco.sql
+++ banco/banco.sql
@@ -1,1 +1,7 @@
-SELECT * FROM usuario.usuario;
+CREATE DATABASE usuario;
+USE usuario;
+CREATE TABLE usuario(
+	id_usuario int PRIMARY KEY AUTO_INCREMENT,
+	email_users varchar(50),
+    assunto_users varchar(50),
+    descricao_users varchar(50));
